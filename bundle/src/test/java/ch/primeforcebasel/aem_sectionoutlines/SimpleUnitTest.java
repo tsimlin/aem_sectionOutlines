f302d543b65cb09e43d9c05fd3b2e5e168c593f0
Kai Lasar <k.lasar@gmx.de>
2015-05-27 20:20:47 +0200

    initial commit

---
+++ bundle/src/test/java/ch/primeforcebasel/aem_sectionoutlines/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package ch.primeforcebasel.aem_sectionoutlines;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
