bb9114347cd75a0c22586ea4b9fdfa4acffd6f40
Thomas Simlinger <thomas.simlinger@prime-force.com>
2015-06-20 15:15:26 +0200

    namespace

--- content/src/main/content/jcr_root/etc/designs/primeforce/clientlib_outlines/js/outlines.js
+++ content/src/main/content/jcr_root/etc/designs/primeforce/clientlib_outlines/js/outlines.js
@@ -1,43 +1,46 @@
-// Replacement for nicer Titles
-var sectionNames = {
-		"accordion": "Accordion",
-		"accordionitem":"Accordion Item",
-		"carousel":"Carousel",
-		"downloaditem":"Downloaditem"
-}
-
-$(document).ready(initTitleSpans);
-
-//create title spans
-function initTitleSpans () {
-	$.each($("div.section"), function () {
-		processDivForTitleSpan(this);
-	});
+var pfOutlines = {
+	sectionNames: {
+			"accordion": "Accordion",
+			"accordionitem":"Accordion Item",
+			"carousel":"Carousel",
+			"downloaditem":"Downloaditem"
+	},
 	
-	$( document ).on( "DOMNodeInserted", function( e ) {
-		if($(e.target).hasClass("section")) { 
-			processDivForTitleSpan($(e.target));
+	initTitleSpans : function () {
+		$.each($("div.section"), function () {
+			pfOutlines.processDivForTitleSpan(this);
+		});
+		
+		pfOutlines.checkNewSections();
+	}, 
+	
+	processDivForTitleSpan: function (obj) {
+		var classNames = $(obj).attr("class").split(" ");
+		
+		if(!$(obj).hasClass("new") && $(obj).find(".compTitle").length == 0) {
+			pfOutlines.addTitleSpan(obj, pfOutlines.getTitleByClasses(classNames));
 		}
-	});
-}
-
-function processDivForTitleSpan(obj) {
-	var classNames = $(obj).attr("class").split(" ");
+	},
 	
-	if(!$(obj).hasClass("new") && $(obj).find(".compTitle").length == 0) {
-		addTitleSpan(obj, getTitleByClasses(classNames));
+	getTitleByClasses: function (classNames) {
+		for (item in classNames) {
+			if(pfOutlines.sectionNames.hasOwnProperty(classNames[item])) {
+				return pfOutlines.sectionNames[classNames[item]];
+			} 
+		}
+		return classNames;
+	},
+	
+	checkNewSections: function () {
+		$( document ).on( "DOMNodeInserted", function( e ) {
+			if($(e.target).hasClass("section")) { 
+				pfOutlines.processDivForTitleSpan($(e.target));
+			}
+		});
+	},
+	
+	addTitleSpan: function (obj,txt) {
+		$(obj).prepend("<span class='compTitle'>" + txt + "</span>");
 	}
-}
-
-function getTitleByClasses(classNames) {
-	for (item in classNames) {
-		if(sectionNames.hasOwnProperty(classNames[item])) {
-			return sectionNames[classNames[item]];
-		} 
-	}
-	return classNames;
-}
-
-function addTitleSpan(obj,txt) {
-	$(obj).prepend("<span class='compTitle'>" + txt + "</span>");
-}
+};
+pfOutlines.initTitleSpans();
